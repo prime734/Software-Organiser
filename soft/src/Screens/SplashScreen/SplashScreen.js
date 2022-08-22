30522d2aa22476ef5359f37351dfb0c73362865f
Thulasizwe <israelsabela@gmail.com>
2022-08-22 20:49:34 +0200

    Description added to Splash

--- soft/src/Screens/SplashScreen/SplashScreen.js
+++ soft/src/Screens/SplashScreen/SplashScreen.js
@@ -22,11 +22,25 @@ function SplashScreen() {
         <Header />
       </div>
       <div class="body">
-        <p>Project Management</p>
+        <p class="title">PROJECT MANAGEMENT</p>
+        <p>Stay organized and connected</p>
+        <p class="description">
+          Bring your team’s work together in one shared space. Choose the
+          project view that suits your style, and collaborate no matter where
+          you are.
+        </p>
         <img height={350} src={project_management} alt="project_management" />
-        <p>Measure your productivity</p>
+        <p class="title">Measure your productivity</p>
+        <p>The Best Tool to Measure Productivity</p>
+        <p class="description"></p>
         <img height={350} src={productivity} alt="productivity" />
-        <p>Kanban board</p>
+        <p class="title">Kanban board</p>
+        <p>It’s more than work. It’s a way of working together.</p>
+        <p class="description">
+          Start with a board, lists, and cards. Customize and expand with more
+          features as your teamwork grows. Manage projects, organize tasks, and
+          build team spirit—all in one place.
+        </p>
         <img height={350} src={kanban} alt="kanban" />
         <p>Add user stories</p>
         <img height={350} src={user_story} alt="user_story" />
