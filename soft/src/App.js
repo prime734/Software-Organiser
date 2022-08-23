6cf40e8edce01553cf59d92093fb894af98aceab
Tshepiso Mahoko <2352695@students.wits.ac.za>
2022-08-23 20:57:29 +0200

    commit

--- soft/src/App.js
+++ soft/src/App.js
@@ -6,12 +6,13 @@ import {
 
 import SplashScreen from './Screens/SplashScreen/SplashScreen';
 import Page2 from "./Screens/Page2/Page2";
+import Login from "./Screens/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SplashScreen />} />
+        <Route path="/" element={<Login />} />
         <Route path="page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
---
+++ soft/src/Screens/Login/Login.js
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import './Login.css';
+
+
+function Login() {
+    let navigate = useNavigate();
+
+    const routeChange = () => {
+        let path = '/';
+        navigate(path);
+    }
+
+    return (
+        <div>
+            <div class="header"><Header /></div>
+            <div className="centre"><br/>
+                <input className='login-input' placeholder="Email"/><br/>
+                <input className='login-input'  placeholder="Password" /><br />
+                <button className='forget-password-button'>Forgot Password?</button><br/>
+                <button class="Loginbutton">Login</button>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Login;
