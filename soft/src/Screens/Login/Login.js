6b4e48aa00042804c60e4c23f4a2f53a7e941fc4
Tshepiso Mahoko <2352695@students.wits.ac.za>
2022-08-24 16:42:09 +0200

    login

--- soft/src/Screens/Login/Login.js
+++ soft/src/Screens/Login/Login.js
@@ -1,16 +1,32 @@
 import { useNavigate } from 'react-router-dom';
-
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import './Login.css';
+import { loginmethod} from '../../firebase';
 
 
 function Login() {
     let navigate = useNavigate();
-
+    let password , email;
     const routeChange = () => {
         let path = '/';
         navigate(path);
+    }
+    const Setemail = event => {
+        email = event.target.value; 
+    }
+    const Setpassword = event => {
+        password = event.target.value;     
+    }
+    const OnLogin = () => {
+        //send a function to the database to check if log the user in
+        loginmethod(email, password);
+
+        
+    }
+
+    const OnForgot = () => {
+        // need to do something if the user forgot password
     }
 
     return (
@@ -18,17 +34,16 @@ function Login() {
             <div class="header"><Header /></div>
             <div className='container'>
                 <div className='resize'>
-                   
                     <div className="centre"><br />         
-                        <input className='login-input' placeholder="Email"/><br/>
-                        <input className='login-input'  placeholder="Password" /><br/>
+                        <input className='login-input' placeholder="Email" onChange={Setemail}/><br/>      
+                        <input className='login-input'  placeholder="Password" onChange={Setpassword}/> <br/>
                         <button className='forget-password-button'>Forgot Password?</button><br/>
-                        <button class="Loginbutton">Login</button>
+                        <button class="Loginbutton" onClick={OnLogin}>Login</button>
                     </div>
                 </div>
                 <div className='background'></div>
             </div>
-            
+        
         </div>
     );
 }
