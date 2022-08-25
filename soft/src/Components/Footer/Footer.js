b539dd1786f85b7b20dbd269b187f93b98dd4038
Thulasizwe <israelsabela@gmail.com>
2022-08-25 15:13:25 +0200

    Footer updated

--- soft/src/Components/Footer/Footer.js
+++ soft/src/Components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import "./Footer.css";
+import Typography from "@mui/material/Typography";
 
 function Footer() {
   return (
@@ -7,12 +8,11 @@ function Footer() {
         <p className="footer-text">
           Welcome to Wits Software Organiser where we will be designing and
           implementing the Kanban-based platform for organizing large software
-          projects, in the vein of Taiga or Trello, you agree to have read and
+          projects, in the vein of Taiga or Trello. You agree to have read and
           accepted our terms of use, cookie and privacy policy.
         </p>
         <p className="footer-text">
-          Copyright 1999-2022 by Refsnes Data. All Rights Reserved. W3Schools is
-          Powered by W3.CSS.
+          Copyright&copy; 1999-2022 by Lion Data. All Rights Reserved.
         </p>
       </footer>
     </div>
