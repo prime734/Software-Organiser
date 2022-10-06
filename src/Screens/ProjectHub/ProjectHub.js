31f37be8f35c5a2c206a4f54bfcb9d4604422e51
Thulasizwe <israelsabela@gmail.com>
2022-10-06 15:16:11 +0200

    Adding members initiated

--- src/Screens/ProjectHub/ProjectHub.js
+++ src/Screens/ProjectHub/ProjectHub.js
@@ -13,9 +13,7 @@ import Wiki from "../../Components/Wiki/Wiki";
 
 function ProjectHub() {
   const { state } = useLocation();
-  const {
-    pName,
-  } = state;      //state from previous page
+  const { pName } = state; //state from previous page
   let navigate = useNavigate();
   const { userEmail, setUserEmail } = useContext(EmailContext);
   const projectRef = collection(db, "Projects");
@@ -23,14 +21,10 @@ function ProjectHub() {
   const [toggleState, setToggleState] = useState(1);
   const [loading, setLoading] = useState(true);
 
-
   ////////////////////////////////////////////////////////////////
 
   useEffect(() => {
-
-
     const getProject = async () => {
-
       const q = query(projectRef, where("ProjectName", "==", pName));
 
       const querySnapshot = await getDocs(q);
@@ -40,12 +34,10 @@ function ProjectHub() {
 
         //console.log('Document data:', doc.data());
       });
-
-    }
+    };
 
     getProject();
-
-  },[]);
+  }, []);
 
   const goBack = () => {
     navigate(-1);
@@ -64,13 +56,16 @@ function ProjectHub() {
     // });
   }
 
-  const deleteProject = async () => {     //handles delete on single project
+  const deleteProject = async () => {
+    //handles delete on single project
     //await deleteDoc(doc(db, "Projects", ID));
     goBack();
   };
 
-  const tryDelete = () => {     //confrms intention to delete a single project
-    let isExecuted = window.confirm(    //pop up to confirm deletion
+  const tryDelete = () => {
+    //confrms intention to delete a single project
+    let isExecuted = window.confirm(
+      //pop up to confirm deletion
       "Are you sure you want to delete this project?"
     );
     if (isExecuted) {
@@ -78,7 +73,8 @@ function ProjectHub() {
     }
   };
 
-  const goAdd = () => {   //navigates to adding stories
+  const goAdd = () => {
+    //navigates to adding stories
     let path = "/add";
     // navigate(path, {
     //   state: {
@@ -103,68 +99,107 @@ function ProjectHub() {
       </div>
       <div class="body">
         <h3>{pName}</h3>
-        <br/>
+        <br />
         <div className="bloc-tabs">
-          <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>
-            <span class="material-symbols-outlined">
-              checklist
-            </span>
-            <br/>
+          <button
+            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            <span class="material-symbols-outlined">checklist</span>
+            <br />
             User Stories
           </button>
-          <button className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>
-            <span class="material-symbols-outlined">
-              view_kanban
-            </span>
-            <br/>
+          <button
+            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+            <span class="material-symbols-outlined">view_kanban</span>
+            <br />
             Kanban
           </button>
-          <button className={toggleState === 3 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(3)}>
-            <span class="material-symbols-outlined">
-              import_contacts
-            </span>
-            <br/>
+          <button
+            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(3)}
+          >
+            <span class="material-symbols-outlined">import_contacts</span>
+            <br />
             Wiki
           </button>
-          <button className={toggleState === 4 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(4)}>
-            <span class="material-symbols-outlined">
-              insights
-            </span>
+          <button
+            className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(4)}
+          >
+            <span class="material-symbols-outlined">insights</span>
             <br />
             Insights
           </button>
-          <button className={toggleState === 5 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(5)}>
-            <span class="material-symbols-outlined">
-              settings
-            </span>
-            <br/>
+          <button
+            className={toggleState === 6 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(6)}
+          >
+            <span class="material-symbols-outlined">groups</span>
+            <br />
+            Members
+          </button>
+          <button
+            className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(5)}
+          >
+            <span class="material-symbols-outlined">settings</span>
+            <br />
             Settings
           </button>
         </div>
 
         <div className="content-tabs">
-          <div className={toggleState === 1 ? "content  active-content" : "content"}>
-            {loading ? null : <UserStories project = {project} />}
+          <div
+            className={
+              toggleState === 1 ? "content  active-content" : "content"
+            }
+          >
+            {loading ? null : <UserStories project={project} />}
           </div>
 
-          <div className={toggleState === 2 ? "content  active-content" : "content"}>
+          <div
+            className={
+              toggleState === 2 ? "content  active-content" : "content"
+            }
+          >
             <p>Kanban</p>
           </div>
 
-          <div className={toggleState === 3 ? "content  active-content" : "content"}>
+          <div
+            className={
+              toggleState === 3 ? "content  active-content" : "content"
+            }
+          >
             {loading ? null : <Wiki project={project} />}
           </div>
 
-          <div className={toggleState === 4 ? "content  active-content" : "content"}>
+          <div
+            className={
+              toggleState === 4 ? "content  active-content" : "content"
+            }
+          >
             <p>Insights</p>
           </div>
 
-          <div className={toggleState === 5 ? "content  active-content" : "content"}>
+          <div
+            className={
+              toggleState === 6 ? "content  active-content" : "content"
+            }
+          >
+            <p>Insights</p>
+          </div>
+
+          <div
+            className={
+              toggleState === 5 ? "content  active-content" : "content"
+            }
+          >
             <p>Settings</p>
           </div>
         </div>
-
-
       </div>
       <div>
         <Footer />
