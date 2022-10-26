65141bb97db265c9906dd0097c9ea0c0a6f01689
Thulasizwe <israelsabela@gmail.com>
2022-10-26 12:27:16 +0200

    Other links added

--- src/Screens/Community/Community.js
+++ src/Screens/Community/Community.js
@@ -2,7 +2,12 @@ import React from "react";
 import HeaderB from "../../Components/HeaderB/HeaderB";
 import "./Community.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import { Container } from "react-bootstrap";
 
 function Community() {
@@ -10,9 +15,18 @@ function Community() {
     <div>
       <HeaderB />
       <Container>
-        <div className="social-container">
+        <div className="social-container d-flex justify-content-evenly">
           <a href="https://facebook.com">
             <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://instagram.com">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://twitter.com">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="https://linkedin.com">
+            <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </div>
       </Container>
