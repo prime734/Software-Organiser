7502f07871e0b1611b99f7cef012222fcb10c553
Thulasizwe <israelsabela@gmail.com>
2022-10-26 15:46:32 +0200

    Successfully added the lion

--- src/Screens/Intro/Intro.js
+++ src/Screens/Intro/Intro.js
@@ -1,14 +1,29 @@
 import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Lottie from "lottie-react";
+import Lion from "../../images/Lion.json";
 
 function Intro() {
   let navigate = useNavigate();
   setTimeout(() => {
     let path = "/splash";
     navigate(path);
-  }, 5000); // render for 5 seconds and then push to home
+  }, 2515); // render for 5 seconds and then push to home
 
-  return <h1>render for 5 seconds and then push to home</h1>;
+  return (
+    <div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+
+      <Lottie animationData={Lion} loop={true} style={{ height: 400 }} />
+    </div>
+  );
 }
 
 export default Intro;
