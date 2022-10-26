214528d10ebd7bb665fb1fc5256432e455294b37
Thulasizwe <israelsabela@gmail.com>
2022-10-26 16:04:59 +0200

    Conflict solved

--- src/App.js
+++ src/App.js
@@ -18,12 +18,9 @@ import AddMember from "./Screens/AddMember/AddMember";
 import ForgotPass from "./Screens/ForgotPass/ForgotPass";
 import AddDocument from "./Screens/AddDocument/AddDocument";
 import ShowDocument from "./Screens/ShowDocument/ShowDocument";
-<<<<<<< HEAD
 import Community from "./Screens/Community/Community";
 import CommunityB from "./Screens/CommunityB/CommunityB";
-=======
 import Intro from "./Screens/Intro/Intro";
->>>>>>> Load
 
 function App() {
   const [userEmail, setUserEmail] = useState("");
