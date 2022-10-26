b742f3e130813825a588bdde4460d118fb08f3bd
Thulasizwe <israelsabela@gmail.com>
2022-10-26 12:18:54 +0200

    Correct import + Facebook

--- src/Screens/Community/Community.js
+++ src/Screens/Community/Community.js
@@ -1,8 +1,23 @@
 import React from "react";
+import HeaderB from "../../Components/HeaderB/HeaderB";
 import "./Community.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { Container } from "react-bootstrap";
 
 function Community() {
-  return <div>Community</div>;
+  return (
+    <div>
+      <HeaderB />
+      <Container>
+        <div className="social-container">
+          <a href="https://facebook.com">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default Community;
