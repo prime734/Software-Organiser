1a009287f504feaf285ba1b20e658842539ca201
Thulasizwe <israelsabela@gmail.com>
2022-10-26 13:18:22 +0200

    Correct social colors

--- src/Screens/Community/Community.js
+++ src/Screens/Community/Community.js
@@ -14,22 +14,56 @@ function Community() {
   return (
     <div>
       <HeaderB />
-      <Container>
-        <div className="social-container d-flex justify-content-evenly">
-          <a href="https://facebook.com">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a href="https://instagram.com">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a href="https://twitter.com">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="https://linkedin.com">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
+      <div class="body">
+        <div class="side-by-side">
+          <div>
+            <p class="title">
+              Be part of the conversation&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>
+              There are conversations about motion happening
+              &nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>
+              everywhere, and we’d like you to add your voice.
+              &nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>
+              Find it all here, whether you are looking for
+              insights&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>
+              from experts, casual chats, live events, tutorials, or
+              &nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>community stories. &nbsp;&nbsp;&nbsp;&nbsp;</p>
+          </div>
+          <div>
+            <p>&nbsp;</p>
+
+            <Container>
+              <div className="social-container">
+                <a className="facebook" href="https://facebook.com">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="instagram" href="https://instagram.com">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="twitter" href="https://twitter.com">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="linkedin" href="https://linkedin.com">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </div>
+            </Container>
+          </div>
         </div>
-      </Container>
+      </div>
+      <p class="title">PROJECT MANAGEMENT</p>
     </div>
   );
 }
